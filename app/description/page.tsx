6b90e6dd0a80b2b3135228ce920d66a7c97acd71
massu-159 <magjtwjp@gmail.com>
2023-10-10 22:12:40 +0900

    fix description

---
+++ app/description/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Description</h1>
+      <p className="text-xl">This is the description page</p>
+    </div>
+  )
+}
