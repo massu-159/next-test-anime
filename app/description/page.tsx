cc01f524f4b93352f7e16557f4dc6651f5304f5b
massu-159 <magjtwjp@gmail.com>
2023-10-26 20:31:50 +0900

    あああああああ

--- app/description/page.tsx
+++ app/description/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
       <h1 className="text-3xl font-bold">Description</h1>
       <p className="text-xl my-8">This is the description page</p>
       <div className="w-40 flex bg-white justify-center items-center rounded-lg">
-        <a href="/" className="text-black">Go back to home</a>
+        <a href="/" className="text-black">Go back to homaaa
+          e</a>
       </div>
     </div>
   )
